bbf03473f3cb47191bc33a36bd6f5f302bd4eff7
Luvuyo <bhekiluvuyo@gmail.com>
2024-08-23 11:48:54 +0200

    linked the backend with user interface

--- express.js
+++ express.js
@@ -10,3 +10,16 @@ function express () {
         
     }
 }
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public')); // Serve static files from the 'public' directory
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Serve the index.html file
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
